1faf132c2c74df28a6995d7a42c8152fc4d38be3
vlkttlv <vitavitavita04@bk.ru>
2025-02-10 21:31:08 +0700

    комит

--- src/db_train_moseld.py
+++ src/db_train_moseld.py
@@ -2,7 +2,7 @@ from src.database import Base
 from sqlalchemy import Column, Computed, Date, DateTime, Integer, ForeignKey, String
 from sqlalchemy.orm import relationship
 
-
+######## АВИАПЕРЕЛЁТЫ ##########
 class Trip(Base):
 
     __tablename__ = "Trip"
@@ -40,3 +40,42 @@ class Passenger(Base):
 
     id = Column(Integer, ForeignKey('Trip.company'), primary_key=True, nullable=False)
     name = Column(String, nullable=False) 
+
+######## СЕМЬЯ ##########
+class FamilyMembers(Base):
+
+    __tablename__ = "FamilyMembers"
+
+    member_id = Column(Integer, primary_key=True, nullable=False)
+    status = Column(String, nullable=False) 
+    member_bame = Column(String, nullable=False) 
+    birthday = Column(DateTime, nullable=False) 
+
+
+class GoodTypes(Base):
+
+    __tablename__ = "GoodTypes"
+
+    good_type_id = Column(Integer, primary_key=True, nullable=False)
+    good_time_name = Column(String, nullable=False) 
+
+
+class Goods(Base):
+
+    __tablename__ = "Goods"
+
+    good_id = Column(Integer, primary_key=True, nullable=False)
+    good_name = Column(String, nullable=False) 
+    type = Column(String, ForeignKey("GoodTypes.good_type_id"), nullable=False) 
+
+
+class Payments(Base):
+
+    __tablename__ = "Payments"
+
+    payment_id = Column(Integer, primary_key=True, nullable=False)
+    family_member = Column(String, ForeignKey("FamilyMembers.member_id"), nullable=False) 
+    good = Column(String, ForeignKey("Goods.good_id"), nullable=False)
+    amount = Column(Integer, nullable=False)
+    unit_price = Column(Integer, nullable=False)
+    date = Column(DateTime, nullable=False)
